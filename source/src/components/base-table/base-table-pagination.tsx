e7db68d990ac86f81239bc3d975a6dc1177d8843
BoundHao <bound.hao@itlvn.com>
2024-08-30 14:06:58 +0700

    fix: Update footer Row selected

--- source/src/components/base-table/base-table-pagination.tsx
+++ source/src/components/base-table/base-table-pagination.tsx
@@ -25,7 +25,7 @@ function BaseTableClientPagination<T extends IBaseData<T>>() {
   return (
     <div className='flex items-center justify-between px-2 '>
       <div className='flex-1 text-sm text-muted-foreground'>
-        {tableConfigContext.table!.getFilteredSelectedRowModel().rows.length} of {tableConfigContext.table!.getFilteredRowModel().rows.length} row(s) selected.
+        {tableConfigContext.isShowSelectionColumn && <span>{tableConfigContext.table!.getFilteredSelectedRowModel().rows.length} of {tableConfigContext.table!.getFilteredRowModel().rows.length} row(s) selected.</span>}
       </div>
       <div className='flex items-center space-x-6 lg:space-x-8'>
         <div className='flex items-center space-x-2'>
@@ -171,7 +171,7 @@ function BaseTableServerPagination<T extends IBaseData<T>>() {
   return (
     <div className='flex items-center justify-between px-2 '>
       <div className='flex-1 text-sm text-muted-foreground'>
-        {tableConfigContext.table!.getFilteredSelectedRowModel().rows.length} of {tableConfigContext.table!.getFilteredRowModel().rows.length} row(s) selected.
+        {tableConfigContext.isShowSelectionColumn && <span>{tableConfigContext.table!.getFilteredSelectedRowModel().rows.length} of {tableConfigContext.table!.getFilteredRowModel().rows.length} row(s) selected.</span>}
       </div>
       <div className='flex items-center space-x-6 lg:space-x-8'>
         <div className='flex items-center space-x-2'>
